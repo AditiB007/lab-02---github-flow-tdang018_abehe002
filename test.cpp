39000b0a2781f47cee7c873a7619112a9a220de2
tdang93 <aznnoodle101@hotmail.com>
2021-04-05 14:45:27 -0700

    [Issue] Migrate lab-01
    * Moved files from lab-01 repo to lab-02 repo

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include "c-echo.h"
+
+#include "gtest/gtest.h"
+
+TEST(EchoTest, HelloWorld) {
+    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    EXPECT_EQ("hello world", echo(3,test_val));
+}
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+//New Test #1
+TEST(EchoTest, Symbols) {
+    char* test_val[2]; test_val[1]; test_val[0] = "./c-echo"; test_val[1] = "~!@#$%^&*()_+";
+    EXPECT_EQ("~!@#$%^&*()_+", echo(2 ,test_val));
+}
+
+//New Test #2
+TEST(EchoTest, Newline) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "test\n";
+    EXPECT_EQ("test\n", echo(2,test_val));
+}
+
+//New Test #3
+TEST(EchoTest, Quotes) {
+    char* test_val[12]; test_val[0] = "./c-echo"; test_val[1] = "This"; test_val[2] = "is"; test_val[3] = "in"; test_val[4] = "double"; test_val[5] = "quotes,"; test_val[6] = "'and"; test_val[7] = "this"; test_val[8] = "is"; test_val[9] = "in"; test_val[10] = "single"; test_val[11] = "quotes.'";
+    EXPECT_EQ("This is in double quotes, 'and this is in single quotes.'", echo(12,test_val));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
